fd295b41e480db2c523bb831b7bc05d9ecaed7c2
s27849 <grzegorzkoziar@outlook.com>
2025-03-27 22:23:48 +0100

    costam

--- app.js
+++ app.js
@@ -25,22 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.use('/download', async (req, res) => {
   const { url } = req.query;
 
@@ -77,6 +61,22 @@ app.use('/download', async (req, res) => {
     console.error('Error streaming:', err);
     res.status(500).send('Failed to stream audio');
   }
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+
+});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+
 });
 
 
